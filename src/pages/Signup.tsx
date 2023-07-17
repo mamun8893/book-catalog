0eb706958a48c683b17cb814bdcb8f7ba2885d90
mamun8893 <mamunurr8893@gmail.com>
2023-07-18 00:28:17 +0600

    hook form integration

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,18 +1,47 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
   return (
     <div className="auth-wrap">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h3>Create Account</h3>
 
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Name"
+            {...register("name", { required: true })}
+          />
+          {errors.name && <span>This field is required</span>}
+        </div>
         <div className="form-group">
           <label>Email</label>
           <input
             type="email"
             className="form-control"
             placeholder="Enter email"
+            {...register("email", { required: true })}
           />
+          {errors.email && <span>This field is required</span>}
         </div>
         <div className="form-group">
           <label>Password</label>
@@ -20,16 +49,11 @@ const Signup = () => {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            {...register("password", { required: true })}
           />
+          {errors.password && <span>This field is required</span>}
         </div>
-        <div className="form-group">
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter confirm password"
-          />
-        </div>
+
         <button type="submit">Sign up</button>
         <p>
           Already have an account <Link to="/login">Login</Link>
