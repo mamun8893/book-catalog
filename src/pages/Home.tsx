b382873fc0ba239aaa26355660f9f0e2196199a9
mamun8893 <mamunurr8893@gmail.com>
2023-07-17 21:16:22 +0600

    banner design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="banner" style={{ backgroundImage: `url(/banner-bg.jpg)` }}>
-      <h2>Home</h2>
+      <div className="container">
+        <div className="banner-wrapper">
+          <div className="banner-left">
+            <div className="content">
+              <p>The Bookworm Editors'</p>
+              <h2>
+                Featured Books of the <span>February</span>
+              </h2>
+              <Link to="/">See More</Link>
+            </div>
+          </div>
+          <div className="banner-right">
+            <img src="/banner-img.png" alt="banner" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
